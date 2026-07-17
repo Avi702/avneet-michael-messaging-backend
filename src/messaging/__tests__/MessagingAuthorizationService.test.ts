6afffb3ce53419a66664a25b885030e79464cded
Michael Rothkopf <michaelhrothkopf@gmail.com>
2026-07-17 10:33:32 -0400

    test: restructure messaging authorization service tests

--- src/messaging/__tests__/MessagingAuthorizationService.test.ts
+++ src/messaging/__tests__/MessagingAuthorizationService.test.ts
@@ -5,78 +5,152 @@ import { MessagingRepository } from "../MessagingRepository";
 
 import { beforeAll, afterAll, describe, beforeEach, test, expect } from "@jest/globals";
 import { MessagingAction, MessagingAuthorizationService } from "../MessagingAuthorizationService";
+import { Chat } from "../Chat.types";
+import { Message } from "../Message.types";
+
+const OID_1 = new mongoose.Types.ObjectId("ffffffffffffffffffffffff");
+const OID_2 = new mongoose.Types.ObjectId("efffffffffffffffffffffff");
+const OID_3 = new mongoose.Types.ObjectId("dfffffffffffffffffffffff");
+
+const GENERIC_CHAT_CREATION_DTO = {
+    title: "Hello",
+};
 
 describe("MessagingRepository", () => {
     let mongo: MongoMemoryServer;
     let repository: MessagingRepository;
     let authorization: MessagingAuthorizationService;
+    let chat: Chat;
+    let message: Message;
     beforeAll(async () => {
         mongo = await MongoMemoryServer.create();
         await mongoose.connect(mongo.getUri());
         repository = new MessagingRepository();
         authorization = new MessagingAuthorizationService(repository);
+        chat = await repository.createChat(OID_1.toString(), GENERIC_CHAT_CREATION_DTO);
+        await repository.addMemberToChat(chat._id.toString(), OID_2.toString());
+        message = await repository.sendMessage(chat._id.toString(), OID_2.toString(), {
+            textContent: "hello"
+        });
     });
     afterAll(async () => {
         await mongoose.disconnect();
         await mongo.stop();
     });
 
-    beforeEach(async () => {
-        await mongoose.connection.collection("chats").deleteMany({});
-        await mongoose.connection.collection("messages").deleteMany({});
-        await mongoose.connection.collection("images").deleteMany({});
-    });
+    describe("member-locked actions", () => {
+        describe("MessagingAction.GetChat", () => {
+            test("accepts user owns chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.GetChat)).toBe(true);
+            });
 
-    const OID_1 = new mongoose.Types.ObjectId("ffffffffffffffffffffffff");
-    const OID_2 = new mongoose.Types.ObjectId("efffffffffffffffffffffff");
-    const OID_3 = new mongoose.Types.ObjectId("dfffffffffffffffffffffff");
+            test("accepts user member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.GetChat)).toBe(true);
+            });
 
-    const GENERIC_CHAT_CREATION_DTO = {
-        title: "Hello",
-    };
-
-    test("authorizes member-locked actions", async () => {
-        const chat = await repository.createChat(OID_1.toString(), GENERIC_CHAT_CREATION_DTO);
-        await repository.addMemberToChat(chat._id.toString(), OID_2.toString());
-
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.GetChat)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.GetChat)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.GetChat)).toBe(false);
-
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.SendMessage)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.SendMessage)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.SendMessage)).toBe(false);
-
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.GetImage)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.GetImage)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.GetImage)).toBe(false);
-
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.GetMessages)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.GetMessages)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.GetMessages)).toBe(false);
-    });
-
-    test("authorizes owner-locked actions", async () => {
-        const chat = await repository.createChat(OID_1.toString(), GENERIC_CHAT_CREATION_DTO);
-        await repository.addMemberToChat(chat._id.toString(), OID_2.toString());
-        const message = await repository.sendMessage(chat._id.toString(), OID_2.toString(), {
-            textContent: "hello"
+            test("rejects user not member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.GetChat)).toBe(false);
+            });
         });
 
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.AddMember)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.AddMember)).toBe(false);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.AddMember)).toBe(false);
+        describe("MessagingAction.SendMessage", () => {
+            test("accepts user owns chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.SendMessage)).toBe(true);
+            });
 
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.RemoveMember)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.RemoveMember)).toBe(false);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.RemoveMember)).toBe(false);
+            test("accepts user member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.SendMessage)).toBe(true);
+            });
 
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.UpdateInformation)).toBe(true);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.UpdateInformation)).toBe(false);
-        expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.UpdateInformation)).toBe(false);
+            test("rejects user not member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.SendMessage)).toBe(false);
+            });
+        });
 
-        expect(await authorization.authorizeAction(message._id.toString(), OID_1.toString(), MessagingAction.UploadImage)).toBe(false);
-        expect(await authorization.authorizeAction(message._id.toString(), OID_2.toString(), MessagingAction.UploadImage)).toBe(true);
-        expect(await authorization.authorizeAction(message._id.toString(), OID_3.toString(), MessagingAction.UploadImage)).toBe(false);
+        describe("MessagingAction.GetImage", () => {
+            test("accepts user owns chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.GetImage)).toBe(true);
+            });
+
+            test("accepts user member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.GetImage)).toBe(true);
+            });
+
+            test("rejects user not member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.GetImage)).toBe(false);
+            });
+        });
+
+        describe("MessagingAction.GetMessages", () => {
+            test("accepts user owns chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.GetMessages)).toBe(true);
+            });
+
+            test("accepts user member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.GetMessages)).toBe(true);
+            });
+
+            test("rejects user not member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.GetMessages)).toBe(false);
+            });
+        });
+    });
+
+    describe("owner-locked actions", () => {
+        describe("MessagingAction.AddMember", () => {
+            test("accepts owner of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.AddMember)).toBe(true);
+            });
+
+            test("rejects member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.AddMember)).toBe(false);
+            });
+
+            test("rejects not member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.AddMember)).toBe(false);
+            });
+        });
+
+        describe("MessagingAction.RemoveMember", () => {
+            test("accepts owner of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.RemoveMember)).toBe(true);
+            });
+
+            test("rejects member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.RemoveMember)).toBe(false);
+            });
+
+            test("rejects not member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.RemoveMember)).toBe(false);
+            });
+        });
+
+        describe("MessagingAction.UpdateInformation", () => {
+            test("accepts owner of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_1.toString(), MessagingAction.UpdateInformation)).toBe(true);
+            });
+
+            test("rejects member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_2.toString(), MessagingAction.UpdateInformation)).toBe(false);
+            });
+
+            test("rejects not member of chat", async () => {
+                expect(await authorization.authorizeAction(chat._id.toString(), OID_3.toString(), MessagingAction.UpdateInformation)).toBe(false);
+            });
+        });
+
+        describe("MessagingAction.UploadImage", () => {
+            test("accepts owner of message", async () => {
+                expect(await authorization.authorizeAction(message._id.toString(), OID_2.toString(), MessagingAction.UploadImage)).toBe(true);
+            });
+
+            test("rejects not owner of message but in chat", async () => {
+                expect(await authorization.authorizeAction(message._id.toString(), OID_1.toString(), MessagingAction.UploadImage)).toBe(false);
+            });
+
+            test("rejects not owner of message and not in chat", async () => {
+                expect(await authorization.authorizeAction(message._id.toString(), OID_3.toString(), MessagingAction.UploadImage)).toBe(false);
+            });
+        });
     });
 });
