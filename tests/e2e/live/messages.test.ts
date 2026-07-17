92baf659ddfe61506caf98af02bd4cc86d7fd3ed
Michael Rothkopf <michaelhrothkopf@gmail.com>
2026-07-17 16:54:41 -0400

    test: fix race condition in messages test for broadcasting messages to other user

--- tests/e2e/live/messages.test.ts
+++ tests/e2e/live/messages.test.ts
@@ -102,12 +102,13 @@ describe("message:send", () => {
         await makeRequest(server.baseUrl, "messaging/addMemberToChat", { chatId: createChatJson._id, userId: userBob.user._id.toString() }, userAlice.accessToken);
         // Subscribe Alice to the chat's live messages
         socketAlice.emit("chat:open", { chatId: createChatJson._id });
+        const promise = extractResponse(socketAlice, "message:new");
         // Send message as Bob; should succeed
         socketBob.emit("message:send", {
             chatId: createChatJson._id,
             textContent: "Hello world",
         });
-        const res = await extractResponse(socketAlice, "message:new");
+        const res = await promise;
         expect(res.success).toBe(true);
         expect(res.message.sender).toStrictEqual(userBob.user._id.toString());
         expect(res.message.textContent).toBe("Hello world");
