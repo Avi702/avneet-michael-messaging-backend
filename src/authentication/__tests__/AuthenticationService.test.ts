1258fcfe769c705b9291c975dfd7efd9b62fcfd4
Michael Rothkopf <michaelhrothkopf@gmail.com>
2026-07-17 10:21:35 -0400

    test: restructure authentication service tests

--- src/authentication/__tests__/AuthenticationService.test.ts
+++ src/authentication/__tests__/AuthenticationService.test.ts
@@ -59,50 +59,68 @@ describe("AuthenticationService", () => {
         password: "password",
     };
 
-    test("registers a new user", async () => {
-        const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
-        expect(user.displayName).toBe(GENERIC_USER_CREATION_DTO.displayName);
+    describe("register method", () => {
+        test("registers a new user", async () => {
+            const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
+            expect(user.displayName).toBe(GENERIC_USER_CREATION_DTO.displayName);
+        });
     });
 
-    test("logs in a registered user", async () => {
-        const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
-        const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
-        expect(loggedIn.user._id.toString()).toBe(user._id.toString());
+    describe("login method", () => {
+        test("logs in a registered user", async () => {
+            const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
+            const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
+            expect(loggedIn.user._id.toString()).toBe(user._id.toString());
+        });
 
-        // does not log in a nonexistent user
-        await expect(authenticationService.login("none", "none")).rejects.toThrow();
-        // does not log in with incorrect password
-        await expect(authenticationService.login(GENERIC_USER_CREATION_DTO.email, "wrong")).rejects.toThrow();
+        test("does not log in a nonexistent user", async () => {
+            await expect(authenticationService.login("none", "none")).rejects.toThrow();
+        });
+
+        test("does not log in with incorrect password", async () => {
+            const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
+            await expect(authenticationService.login(GENERIC_USER_CREATION_DTO.email, "wrong")).rejects.toThrow();
+        });
     });
 
-    test("authenticates based on an access token", async () => {
-        const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
-        const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
-        const authentication = await authenticationService.authenticate(loggedIn.accessToken);
-        expect(authentication._id.toString()).toBe(user._id.toString());
-        // rejection case: invalid token
-        await expect(authenticationService.authenticate("dog")).rejects.toThrow();
+    describe("authenticate method", () => {
+        test("authenticates with an access token", async () => {
+            const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
+            const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
+            const authentication = await authenticationService.authenticate(loggedIn.accessToken);
+            expect(authentication._id.toString()).toBe(user._id.toString());
+        });
+
+        test("rejects invalid token", async () => {
+            await expect(authenticationService.authenticate("dog")).rejects.toThrow();
+        });
     });
 
-    test("refreshses a token", async () => {
-        const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
-        const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
-        const newTokens = await authenticationService.refresh(loggedIn.refreshToken);
-        const authentication = await authenticationService.authenticate(newTokens.accessToken);
-        expect(authentication._id.toString()).toBe(user._id.toString());
-        // rejection case: invalid token
-        await expect(authenticationService.refresh("123")).rejects.toThrow();
+    describe("refresh method", () => {
+        test("refreshes a valid token", async () => {
+            const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
+            const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
+            const newTokens = await authenticationService.refresh(loggedIn.refreshToken);
+            const authentication = await authenticationService.authenticate(newTokens.accessToken);
+            expect(authentication._id.toString()).toBe(user._id.toString());
+        });
+
+        test("rejects invalid token", async () => {
+            await expect(authenticationService.refresh("123")).rejects.toThrow();
+        });
     });
 
-    test("resets a password", async () => {
-        const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
-        const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
-        expect(loggedIn.user._id.toString()).toBe(user._id.toString());
-        await authenticationService.updatePassword(user._id.toString(), "helloworld");
-        // expect old password to fail
-        await expect(authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password)).rejects.toThrow();
-        // expect new password to succeed
-        const newLoggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, "helloworld");
-        expect(newLoggedIn.user._id.toString()).toBe(user._id.toString());
+    describe("updatePassword method", () => {
+        test("updates a password", async () => {
+            const user = await authenticationService.register(GENERIC_USER_CREATION_DTO);
+            const loggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password);
+            expect(loggedIn.user._id.toString()).toBe(user._id.toString());
+            await authenticationService.updatePassword(user._id.toString(), "helloworld");
+            // expect old password to fail
+            await expect(authenticationService.login(GENERIC_USER_CREATION_DTO.email, GENERIC_USER_CREATION_DTO.password)).rejects.toThrow();
+            // expect new password to succeed
+            const newLoggedIn = await authenticationService.login(GENERIC_USER_CREATION_DTO.email, "helloworld");
+            expect(newLoggedIn.user._id.toString()).toBe(user._id.toString());
+        });
     });
 });
