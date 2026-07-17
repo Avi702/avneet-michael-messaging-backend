41357098bc91892420381e272c9482da7f50f4d9
Michael Rothkopf <michaelhrothkopf@gmail.com>
2026-07-16 21:36:19 -0400

    test: add e2e tests for users routes

---
+++ tests/e2e/api/users.test.ts
@@ -0,0 +1,77 @@
+import { beforeAll, afterAll, describe, beforeEach, test, expect } from "@jest/globals";
+import { createTestServer } from "../../../src/manager/createTestServer";
+import { makeRequest } from "../shared/makeRequest";
+import { makeUser } from "../shared/makeUser";
+
+let server: Awaited<ReturnType<typeof createTestServer>>;
+
+beforeAll(async () => {
+    server = await createTestServer();
+});
+
+afterAll(async () => {
+    await server.manager.shutdown();
+});
+
+describe("users routes", () => {
+    describe("getUser endpoint", () => {
+        test("rejects not authenticated", async () => {
+            const res = await makeRequest(server.baseUrl, "users/getUser", {
+                userId: "hello",
+            });
+            expect(res.status).toBe(401);
+            const json = await res.json();
+            expect(json.error.code).toBe("UNAUTHORIZED");
+        });
+
+        test("rejects invalid format", async () => {
+            const loginJson = await makeUser(server.baseUrl);
+            const res = await makeRequest(server.baseUrl, "users/getUser", {
+            }, loginJson.accessToken);
+            expect(res.status).toBe(400);
+            const json = await res.json();
+            expect(json.error.code).toBe("BAD_REQUEST");
+        });
+
+        test("accepts valid input", async () => {
+            const loginJson1 = await makeUser(server.baseUrl);
+            const loginJson2 = await makeUser(server.baseUrl);
+            const res = await makeRequest(server.baseUrl, "users/getUser", {
+                userId: loginJson2.user._id.toString(),
+            }, loginJson1.accessToken);
+            expect(res.status).toBe(200);
+            const json = await res.json();
+            expect(json._id.toString()).toBe(loginJson2.user._id.toString());
+        })
+    });
+
+    describe("updateProfile endpoint", () => {
+        test("rejects not authenticated", async () => {
+            const res = await makeRequest(server.baseUrl, "users/updateProfile", {
+                displayName: "New Name"
+            });
+            expect(res.status).toBe(401);
+            const json = await res.json();
+            expect(json.error.code).toBe("UNAUTHORIZED");
+        });
+
+        test("rejects invalid format", async () => {
+            const loginJson = await makeUser(server.baseUrl);
+            const res = await makeRequest(server.baseUrl, "users/updateProfile", {
+            }, loginJson.accessToken);
+            expect(res.status).toBe(400);
+            const json = await res.json();
+            expect(json.error.code).toBe("BAD_REQUEST");
+        });
+
+        test("accepts valid input", async () => {
+            const loginJson = await makeUser(server.baseUrl);
+            const res = await makeRequest(server.baseUrl, "users/updateProfile", {
+                displayName: "New Name"
+            }, loginJson.accessToken);
+            expect(res.status).toBe(200);
+            const json = await res.json();
+            expect(json.success).toBe(true);
+        });
+    });
+});
