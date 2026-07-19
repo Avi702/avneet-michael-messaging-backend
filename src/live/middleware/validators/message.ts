ad6f28103b8146e2965573cb94d28a6b43c08078
Michael Rothkopf <michaelhrothkopf@gmail.com>
2026-07-18 21:26:13 -0400

    fix: change get messages schema in live API to include all fields and cursorDate/cursorId conditioning

--- src/live/middleware/validators/message.ts
+++ src/live/middleware/validators/message.ts
@@ -10,9 +10,16 @@ export type SendMessageData = z.infer<typeof sendMessageSchema>;
 
 export const getMessagesSchema = z.object({
     chatId: zObjectId,
-    limit: z.number().min(1).max(50),
-    cursorDate: z.date(),
-    cursorId: zObjectId,
+    limit: z.number().min(1).max(50).optional(),
+    cursorDate: z.date().optional(),
+    cursorId: zObjectId.optional(),
+}).refine(data => {
+    const hasCursorDate = !!data.cursorDate;
+    const hasCursorId = !!data.cursorId;
+    return hasCursorDate === hasCursorId;
+}, {
+    message: "Both cursorDate and cursorId or neither must be provided",
+    path: ["cursorDate", "cursorId"],
 });
 
 export type GetMessagesData = z.infer<typeof getMessagesSchema>;
